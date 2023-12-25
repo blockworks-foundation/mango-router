1dd8cc506437413187c3b9b63d07112622796473
Maximilian Schneider <mail@maximilianschneider.net>
2023-12-26 00:05:33 +0100

    make raven mango account configurable

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import { PublicKey } from "@solana/web3.js";
 export const RAVEN_PROGRAM_ADDRESS: string =
   "AXRsZddcKo8BcHrbbBdXyHozSaRGqHc11ePh9ChKuoa1";
 export const RAVEN_MANGO_ACCOUNT: PublicKey = new PublicKey(
-  "GRR9y6yBxfxqVS7xQekRk4c6KUB2ukqSM8fY8GJSSCbo"
+  process.env.RAVEN_MANGO_ACCOUNT || "GRR9y6yBxfxqVS7xQekRk4c6KUB2ukqSM8fY8GJSSCbo"
 );
 export const RAVEN_MANGO_ACCOUNT_OWNER: PublicKey = PublicKey.findProgramAddressSync(
   [Buffer.from("pda")],
